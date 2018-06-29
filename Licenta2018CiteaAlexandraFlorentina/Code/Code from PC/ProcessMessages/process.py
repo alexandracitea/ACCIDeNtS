ff22e001580c525e31144aa0caa02f614c508028
Alexandra Citea <citea_alexandra@yahoo.com>
2018-06-29 18:55:09 +0300

    Uploaded the correct version of process.py

--- Licenta2018CiteaAlexandraFlorentina/Code/Code from PC/ProcessMessages/process.py
+++ Licenta2018CiteaAlexandraFlorentina/Code/Code from PC/ProcessMessages/process.py
@@ -2,20 +2,25 @@ import csv
 import sys
 import os
 import django
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'accidents'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accidents.settings")
+from django.utils import timezone
 from django.conf import settings
+from map.models import Incident
+from django.core.wsgi import get_wsgi_application
 
-from accidents.models import MyModel
+sys.path.append("C://Users//Alexandra Citea//Desktop//Licenta//accidents")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accidents.settings")
+django.setup()
+
+application = get_wsgi_application()
+
 
 file_msg = open("C://Users//Alexandra Citea//Desktop//Messages.txt", "r")
 previous_line1 = file_msg.read().splitlines()
-previous_line=[]
+previous_line = []
 for elem in previous_line1:
     if elem != "":
         previous_line.append(elem)
-print (previous_line)
+print(previous_line)
 file_msg.close()
 while True:
     file_msg = open("C://Users//Alexandra Citea//Desktop//Messages.txt", "r")
@@ -30,8 +35,12 @@ while True:
         new_accident = line[-1]
         print("New accident info: "+new_accident)
         elements = new_accident.split(' ')
-        with open("C://Users//Alexandra Citea//Desktop//MessagesToBeInserted.txt", "w+") as file:
-            wr = csv.writer(file, quoting = csv.QUOTE_ALL)
-            wr.writerow(elements)
-        file.close()
+        print(elements)
+        model = Incident()
+        model.timestamp = timezone.now()
+        model.accidentsPriority = elements[0]
+        model.latitude = elements[1]
+        model.longitude = elements[2]
+        model.save()
+
     file_msg.close()
